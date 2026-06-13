4b58f3a4d39dbb5784a082e824337686866030fa
Prabhat Bhusal <prabhatbhusal777@gmail.com>
2026-06-13 22:54:15 +0545

    frontend setup

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+// import {React,useState} from 'react';
+
+const Navbar=()=>{
+    // const [isOpen,setOpen]=useState(false)
+
+    return(
+        <>
+            <nav className='w-full fixed h-10 flex justify-around flex-wrap items-center bg-amber-800'>
+                <ul>Home</ul>
+                <ul>Analytics</ul>
+                
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,17 @@
+// import {React,useState} from 'react';
+
+// const Home=()=>{
+//     const [isOpen,setOpen]=useState(false)
+
+//     return(
+//         <>
+//             <nav className='w-full fixed h-10 flex justify-center items-center'>
+//                 <li>Home</li>
+//                 <li>Analytics</li>
+                
+//             </nav>
+//         </>
+//     )
+// }
+
+// export default Home
