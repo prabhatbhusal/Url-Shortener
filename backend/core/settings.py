bd06abcea9f7c36aba672ce134f0e9ab1c0755cc
Prabhat Bhusal <prabhatbhusal777@gmail.com>
2026-06-14 19:00:54 +0545

    Finished backend and complete home except Analytics/chart dashboard

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -123,8 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
+CORS_ALLOW_ALL_ORIGINS = True
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,43 +1,102 @@
-import { useState } from "react"
+import { useState, useEffect } from "react";
 
 const Home = () => {
-    const [url, setUrl] = useState("")
-    const [result, setResult] = useState("")
-    const [countdown, setCountdown] = useState(0)
-    function handleChange(e) {
-        setUrl(e.target.value)
+    const [url, setUrl] = useState("");
+    const [result, setResult] = useState("");
+    const [countdown, setCountdown] = useState(0);
+    const [error, setError] = useState(false);
+    useEffect(() => {
+        if (countdown > 0) {
+            const timer = setInterval(() => {
+                setCountdown((prev) => prev - 1);
+            }, 1000);
+            return () => clearInterval(timer);
+        }
+    }, [countdown]);
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setUrl(e.target.value);
     }
-    async function handleSubmit(e) {
-        e.preventDefault()
-        const response = await fetch("http://127.0.0.1:8000/api/shortenurl/", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ url_entry: url },
+    async function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
+        e.preventDefault();
+        setResult("");
+        setError(false);
+        try {
+            const response = await fetch("http://127.0.0.1:8000/api/shortenurl/", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ url_entry: url }),
+            });
+            console.log(response);
+            const data = await response.json();
+            console.log(data);
+            if (response.status == 400) {
+                setResult("The URL has already been shortened");
+                setError(true);
+            } else if (response.status == 429) {
+                setCountdown(data.retry_after);
+            } else if (response.status == 201) {
+                setResult(data.alias_value);
+            }
+        }
+        catch (error) {
+            setResult("Please Try again later:Server/network error")
+            setError(true)
+        }
 
-            )
-        })
-        console.log(response)
     }
 
     return (
         <>
             <section>
-                <form onSubmit={handleSubmit} className="flex items-center justify-center  ">
+                <form
+                    onSubmit={handleSubmit}
+                    className="flex items-center justify-center  "
+                >
                     <div className="p-5 ">
                         <label className="p-5 text-black">Please enter url:</label>
                         <input
                             type="text"
-                            id='Url' value={url}
-                            onChange={handleChange} placeholder="Enter Url" className="p-5 text-black border border-black rounded-2xl" />
+                            id="Url"
+                            value={url}
+                            onChange={handleChange}
+                            placeholder="Enter Url"
+                            className="p-5 text-black border border-black rounded-2xl"
+                        />
                     </div>
-                    <input type="submit" className="py-5 px-10 bg-blue-600 rounded-full hover:ease-in-out hover:transition-all hover:scale-110 hover:duration-500 text-white"
+                    <input
+                        type="submit"
+                        className="py-5 px-10 bg-blue-600 rounded-full hover:ease-in-out hover:transition-all hover:scale-110 hover:duration-500 text-white"
                     />
                 </form>
+                {result && !error && (
+                    <a
+                        href={error ? "#" : `http://127.0.0.1:8000/api/${result}`}
+                        className="text-black justify-center items-center flex"
+                    >
+                        {error ? "" : `127.0.0.1:8000/api/${result}`}
+                    </a>
+                )}
+                {error && (
+                    <p className="text-black justify-center items-center flex">
+                        Url already shortened
+                    </p>
+                )}
+                {countdown > 0 && (
+                    <h2
+                        className={
+                            countdown > 0
+                                ? "visible text-black justify-center items-center flex"
+                                : "hidden"
+                        }
+                    >
+                        {countdown} seconds remaining
+                    </h2>
+                )}
             </section>
         </>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
